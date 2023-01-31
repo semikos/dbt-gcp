9703cb7b1140ed3adf69395d26bdae9073cc5580
Sami <samykobbi@gmail.com>
2023-01-31 17:34:36 +0100

    Initial commit

---
+++ asba.bat
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"Sarra":"Sami"}' \
+ http://127.0.0.1:8080/dbt 
---
+++ setup.bat
@@ -0,0 +1,4 @@
+set GCP_PROJECT_APP="gcp-demo-373915"
+set ENV=sdx
+set DBT_PROFILES_DIR=analytics
+set DBT_PROJECT_DIR=analytics
